e0e85f3c2a087c4097fed2f9f994073e583895d5
Nano Twerpus <nanotwerp@gmail.com>
2025-02-26 20:08:11 -0500

    fix: use `applicationName` instead of `desktopName` in firefox's overlay

--- firefox-overlay.nix
+++ firefox-overlay.nix
@@ -14,37 +14,49 @@ let
   # Open a pull request against https://github.com/mozilla-releng/shipit to
   # update the version, as done in
   # https://github.com/mozilla-releng/shipit/pull/1467
-  firefox_versions = with builtins;
+  firefox_versions =
+    with builtins;
     fromJSON (readFile (fetchurl "https://product-details.mozilla.org/1.0/firefox_versions.json"));
 
-  arch = if self.stdenv.system == "i686-linux"
-    then "linux-i686"
-    else "linux-x86_64";
+  arch = if self.stdenv.system == "i686-linux" then "linux-i686" else "linux-x86_64";
 
-  yearOf = with super.lib; yyyymmddhhmmss:
-    head (splitString "-" yyyymmddhhmmss);
-  monthOf = with super.lib; yyyymmddhhmmss:
-    head (tail (splitString "-" yyyymmddhhmmss));
+  yearOf = with super.lib; yyyymmddhhmmss: head (splitString "-" yyyymmddhhmmss);
+  monthOf = with super.lib; yyyymmddhhmmss: head (tail (splitString "-" yyyymmddhhmmss));
 
   # Given SHA512SUMS file contents and file name, extract matching sha512sum.
-  extractSha512Sum = sha512sums: file:
+  extractSha512Sum =
+    sha512sums: file:
     with builtins;
     # Nix 1.x do not have `builtins.split`.
     # Nix 2.0 have an bug in `builtins.match` (see https://github.com/NixOS/nix/issues/2147).
     # So I made separate logic for Nix 1.x and Nix 2.0.
     if builtins ? split then
-      substring 0 128 (head
-        (super.lib.filter
-          (s: isString s && substring 128 (stringLength s) s == "  ${file}")
-          (split "\n" sha512sums)))
+      substring 0 128 (
+        head (
+          super.lib.filter (s: isString s && substring 128 (stringLength s) s == "  ${file}") (
+            split "\n" sha512sums
+          )
+        )
+      )
     else
       head (match ".*[\n]([0-9a-f]*)  ${file}.*" sha512sums);
 
   # The timestamp argument is a yyyy-mm-dd-hh-mm-ss date, which corresponds to
   # one specific version. This is used mostly for bisecting.
-  versionInfo = { name, version, release, system ? arch, timestamp ? null, info ? null, ... }: with builtins;
-    if (info != null) then info else
-    if release then
+  versionInfo =
+    {
+      name,
+      version,
+      release,
+      system ? arch,
+      timestamp ? null,
+      info ? null,
+      ...
+    }:
+    with builtins;
+    if (info != null) then
+      info
+    else if release then
       # For versions such as Beta & Release:
       # https://download.cdn.mozilla.net/pub/firefox/releases/55.0b3/SHA256SUMS
       let
@@ -56,7 +68,8 @@ let
         extension = if majorVersion > 134 then "tar.xz" else "tar.bz2";
         file = "${system}/en-US/firefox-${version}.${extension}";
         sha512Of = chksum: file: extractSha512Sum (readFile (fetchurl chksum)) file;
-      in rec {
+      in
+      rec {
         chksum = "${dir}/SHA512SUMS";
         chksumSig = "${chksum}.asc";
         chksumSha256 = hashFile "sha256" (fetchurl "${dir}/SHA512SUMS");
@@ -74,13 +87,23 @@ let
         dir =
           if timestamp == null then
             let
-              buildhubJSON = with builtins;
-                fromJSON (readFile (fetchurl "https://download.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central/firefox-${version}.en-US.${system}.buildhub.json"));
-            in builtins.replaceStrings [ "/${file}" ] [ "" ] buildhubJSON.download.url
-          else "https://download.cdn.mozilla.net/pub/firefox/nightly/${yearOf timestamp}/${monthOf timestamp}/${timestamp}-mozilla-central" ;
+              buildhubJSON =
+                with builtins;
+                fromJSON (
+                  readFile (
+                    fetchurl "https://download.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central/firefox-${version}.en-US.${system}.buildhub.json"
+                  )
+                );
+            in
+            builtins.replaceStrings [ "/${file}" ] [ "" ] buildhubJSON.download.url
+          else
+            "https://download.cdn.mozilla.net/pub/firefox/nightly/${yearOf timestamp}/${monthOf timestamp}/${timestamp}-mozilla-central";
         file = "firefox-${version}.en-US.${system}.tar.xz";
-        sha512Of = chksum: file: head (match ".*[\n]([0-9a-f]*) sha512 [0-9]* ${file}[\n].*" (readFile (fetchurl chksum)));
-      in rec {
+        sha512Of =
+          chksum: file:
+          head (match ".*[\n]([0-9a-f]*) sha512 [0-9]* ${file}[\n].*" (readFile (fetchurl chksum)));
+      in
+      rec {
         chksum = "${dir}/firefox-${version}.en-US.${system}.checksums";
         chksumSig = null;
         # file content:
@@ -94,22 +117,31 @@ let
 
   # From the version info, check the authenticity of the check sum file, such
   # that we guarantee that we have
-  verifyFileAuthenticity = { file, sha512, chksum, chksumSig }:
+  verifyFileAuthenticity =
+    {
+      file,
+      sha512,
+      chksum,
+      chksumSig,
+    }:
     assert extractSha512Sum (builtins.readFile chksum) file == sha512;
-    super.runCommand "check-firefox-signature" {
-      buildInputs = [ self.gnupg ];
-      FILE = chksum;
-      ASC = chksumSig;
-    } ''
-      set -eu
-      gpg --dearmor < ${pgpKey} > keyring.gpg
-      gpgv --keyring=./keyring.gpg $ASC $FILE
-      mkdir $out
-    '';
+    super.runCommand "check-firefox-signature"
+      {
+        buildInputs = [ self.gnupg ];
+        FILE = chksum;
+        ASC = chksumSig;
+      }
+      ''
+        set -eu
+        gpg --dearmor < ${pgpKey} > keyring.gpg
+        gpgv --keyring=./keyring.gpg $ASC $FILE
+        mkdir $out
+      '';
 
   # From the version info, create a fetchurl derivation which will get the
   # sources from the remote.
-  fetchVersion = info:
+  fetchVersion =
+    info:
     if info.chksumSig != null then
       super.fetchurl {
         inherit (info) url sha512;
@@ -118,11 +150,19 @@ let
         # verification of the checksum.  Thus, this fetch script can only be
         # executed once the verifyAuthenticity script finished successfully.
         postFetch = ''
-          : # Authenticity Check (${verifyFileAuthenticity {
-            inherit (info) file sha512;
-            chksum = builtins.fetchurl { url = info.chksum; sha256 = info.chksumSha256; };
-            chksumSig = builtins.fetchurl { url = info.chksumSig; sha256 = info.chksumSigSha256; };
-          }})
+          : # Authenticity Check (${
+            verifyFileAuthenticity {
+              inherit (info) file sha512;
+              chksum = builtins.fetchurl {
+                url = info.chksum;
+                sha256 = info.chksumSha256;
+              };
+              chksumSig = builtins.fetchurl {
+                url = info.chksumSig;
+                sha256 = info.chksumSigSha256;
+              };
+            }
+          })
         '';
       }
     else
@@ -133,33 +173,44 @@ let
         # match the signature file. Fortunately, any failure of this code would
         # prevent the output from being reused.
         postFetch =
-          let asc = super.fetchurl { url = info.sig; sha512 = info.sigSha512; }; in ''
-          : # Authenticity Check
-          set -eu
-          export PATH="$PATH:${self.gnupg}/bin/"
-          gpg --dearmor < ${pgpKey} > keyring.gpg
-          gpgv --keyring=./keyring.gpg ${asc} $out
-        '';
+          let
+            asc = super.fetchurl {
+              url = info.sig;
+              sha512 = info.sigSha512;
+            };
+          in
+          ''
+            : # Authenticity Check
+            set -eu
+            export PATH="$PATH:${self.gnupg}/bin/"
+            gpg --dearmor < ${pgpKey} > keyring.gpg
+            gpgv --keyring=./keyring.gpg ${asc} $out
+          '';
       };
 
-  firefoxVersion = version:
+  firefoxVersion =
+    version:
     let
       info = versionInfo version;
-      pkg = ((self.firefox-bin-unwrapped.override {
-        generated = {
-          version = version.version;
-          sources = { inherit (info) url sha512; };
-        };
-        channel = version.channel;
-      }).overrideAttrs (old: {
-        # Add a dependency on the signature check.
-        src = fetchVersion info;
-      }));
-      in super.wrapFirefox pkg {
-        pname = "${pkg.binaryName}-bin";
-        desktopName = version.name;
-        wmClass = version.wmClass;
-      };
+      pkg = (
+        (self.firefox-bin-unwrapped.override {
+          generated = {
+            version = version.version;
+            sources = { inherit (info) url sha512; };
+          };
+          channel = version.channel;
+        }).overrideAttrs
+          (old: {
+            # Add a dependency on the signature check.
+            src = fetchVersion info;
+          })
+      );
+    in
+    super.wrapFirefox pkg {
+      pname = "${pkg.binaryName}-bin";
+      desktopName = version.name or { };
+      wmClass = version.wmClass;
+    };
 
   firefoxVariants = {
     firefox-nightly-bin = {
@@ -196,27 +247,40 @@ in
 {
   lib = super.lib // {
     firefoxOverlay = {
-      inherit pgpKey firefoxVersion versionInfo firefox_versions firefoxVariants;
+      inherit
+        pgpKey
+        firefoxVersion
+        versionInfo
+        firefox_versions
+        firefoxVariants
+        ;
     };
   };
 
   # Set of packages which are automagically updated. Do not rely on these for
   # reproducible builds.
-  latest = (super.latest or {}) // (builtins.mapAttrs (n: v: firefoxVersion v) firefoxVariants);
+  latest = (super.latest or { }) // (builtins.mapAttrs (n: v: firefoxVersion v) firefoxVariants);
 
   # Set of packages which used to build developer environment
-  devEnv = (super.shell or {}) // {
+  devEnv = (super.shell or { }) // {
     gecko = super.callPackage ./pkgs/gecko {
       inherit (self.python38Packages) setuptools;
       pythonFull = self.python38Full;
       nodejs =
-        if builtins.compareVersions self.nodejs.name "nodejs-8.11.3" < 0
-        then self.nodejs-8_x else self.nodejs;
+        if builtins.compareVersions self.nodejs.name "nodejs-8.11.3" < 0 then
+          self.nodejs-8_x
+        else
+          self.nodejs;
 
       rust-cbindgen =
-        if !(self ? "rust-cbindgen") then self.rust-cbindgen-latest
-        else if builtins.compareVersions self.rust-cbindgen.version self.rust-cbindgen-latest.version < 0
-        then self.rust-cbindgen-latest else self.rust-cbindgen;
+        if !(self ? "rust-cbindgen") then
+          self.rust-cbindgen-latest
+        else if
+          builtins.compareVersions self.rust-cbindgen.version self.rust-cbindgen-latest.version < 0
+        then
+          self.rust-cbindgen-latest
+        else
+          self.rust-cbindgen;
 
       # Due to std::ascii::AsciiExt changes in 1.23, Gecko does not compile, so
       # use the latest Rust version before 1.23.
@@ -235,5 +299,5 @@ in
     };
   };
 
-  jsdoc = super.callPackage ./pkgs/jsdoc {};
+  jsdoc = super.callPackage ./pkgs/jsdoc { };
 }
